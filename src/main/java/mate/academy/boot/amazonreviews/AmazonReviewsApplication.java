31188df54fac20cec4fccc83aaf5547ffd723ffe
ihor <krohmal.igor777@gmail.com>
2020-06-23 11:41:51 +0300

    first commit

---
+++ src/main/java/mate/academy/boot/amazonreviews/AmazonReviewsApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.amazonreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/boot/amazonreviews/AmazonReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.amazonreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
