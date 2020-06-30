d217fa235b950afd0468c764862157e69d849144
ihor <krohmal.igor777@gmail.com>
2020-06-30 14:46:19 +0300

    fixed CsvFileParserServiceTest and add Text.txt file

--- src/test/java/mate/academy/boot/amazonreviews/service/CsvFileParserServiceTest.java
+++ src/test/java/mate/academy/boot/amazonreviews/service/CsvFileParserServiceTest.java
@@ -9,16 +9,12 @@ import org.junit.jupiter.api.Test;
 
 class CsvFileParserServiceTest {
     private final CsvFileParserService csvFileParserService = new CsvFileParserService();
+    private final FileReaderService fileReaderService = new FileReaderService();
 
     @Test
     public void parse_Ok() {
-        List<String> reviewStrings = List.of("Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,"
-                +"HelpfulnessDenominator,Score,Time,Summary,Text",
-                "1,B001E4KFG0,A3SGXH7AUHU8GW,delmartian,1,1,5,1303862400,Good Quality Dog Food,I "
-                        + "have bought several of the Vitality canned dog food products and have "
-                        + "found them all to be of good quality. The product looks more like a "
-                        + "stew than a processed meat and it smells better. My Labrador is finicky "
-                        + "and she appreciates this product better than  most.");
+        List<String> reviewStrings = fileReaderService
+                .readFile("src/test/resources/Text.txt");
         ReviewFromFileDto review = new ReviewFromFileDto();
         review.setReviewId(1L);
         review.setProductId("B001E4KFG0");
